b8af7062799bddca82f514b3c90dbca0b58e62f9
1700948 <jaha5825@gmail.com>
2022-05-06 12:16:57 +0100

    Interface almost finished, fun enviaDados created.

--- app/src/main/java/com/example/jogoadivinha/MainActivity.kt
+++ app/src/main/java/com/example/jogoadivinha/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.jogoadivinha
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    private var numeroF = Random(Int.MAX_VALUE)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonAdivinha = findViewById<Button>(R.id.buttonAdivinha)
+        buttonAdivinha.setOnClickListener{  enviaDados()  }
+    }
+
+    fun enviaDados(){
+        val editTextNumber = findViewById<TextView>(R.id.editTextNumber)
+        val numeroAdivinhado = editTextNumber.text.toString().toInt()
+
+        if(numeroAdivinhado > 10){
+            editTextNumber.error = getString(R.string.numero_maior_que_10)
+            editTextNumber.requestFocus()
+            return
+        }
+
     }
 }
